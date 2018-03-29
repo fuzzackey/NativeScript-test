f15f9e90b752e78ca518e803e3535704e084ddde
zaki <zaki.zahri@canang.com.my>
2018-03-29 12:01:07 +0800

    First Commit

---
+++ platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/main.m \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/terang/Documents/nativescriptprojects/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
